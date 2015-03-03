396cb8a7ce03e3f0fe3d35aab5cf3f0c49603edd
liang <lwh@aduu.cn>
2015-03-03 11:39:43 +0800

    add 3 line tips

--- testGit/src/com/example/testgit/MainActivity.java
+++ testGit/src/com/example/testgit/MainActivity.java
@@ -12,6 +12,9 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 		// ok
 		// this is a tips
+		// 1-git add testGit 将修改的文件或则目录添加到暂存区
+		// 2-git commit -m "说明"	 将暂存区提交到本地库
+		// 3-git push origin master 将本地推送到远程库
 	}
 
 	@Override
